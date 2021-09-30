c9da1dfe8ed2054e24d0882665ac9447cb899d2a
george <heorhii.makeiev@gmail.com>
2021-09-30 21:38:36 +0300

    HomeWork1

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-const builder = require('./helper/user-builder');
-
-let user = builder.createUser('Victor', 25);
-
-// console.log(user);
-
 const fs = require('fs');
+const path = require('path');
+
+const appendBoysPath = path.join(__dirname, 'users', 'boys');
+const appendGirlsPath = path.join(__dirname, 'users', 'girls');
+
+
+fs.readdir(appendBoysPath, (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    data.forEach((item) => {
+        fs.readFile( (err, data) => {
+            if(err) {
+                console.log(err);
+                return;
+            }
+                console.log(item)
+        }
+        )
+    } )
+})
+
+
+
+
