84099a8cfe18e77bdbe66ea70b9f0d2a28eb40a7
george <heorhii.makeiev@gmail.com>
2021-10-01 14:59:51 +0300

    HomeWork1

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 const fs = require('fs');
 const path = require('path');
 
-const appendBoysPath = path.join(__dirname, 'users', 'boys');
-const appendGirlsPath = path.join(__dirname, 'users', 'girls');
+const pathToBoys = path.join(__dirname, 'users', 'boys');
+const pathToGirls = path.join(__dirname, 'users', 'girls');
 
-
-fs.readdir(appendBoysPath, (err, data) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    data.forEach((item) => {
-        fs.readFile( (err, data) => {
-            if(err) {
-                console.log(err);
-                return;
-            }
-                console.log(item)
+const sortUsers = (gender, oldFolder, newFolder) => {
+    fs.readdir(oldFolder, (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
         }
-        )
-    } )
-})
 
+        data.forEach((value) => {
+            fs.readFile(path.join(oldFolder, value), (err, data) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
 
+                const user = JSON.parse(data.toString());
+                if (user.gender === gender) {
+                    fs.rename(path.join(oldFolder, value), path.join(newFolder, value), err => {
+                        if (err) {
+                            console.log(err);
+                        }
+                    })
+                }
+            });
+        });
+    });
+};
+
+sortUsers('male', pathToGirls, pathToBoys,);
+sortUsers('female', pathToBoys, pathToGirls);
 
 
