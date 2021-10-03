fed849304007aea69055e2a0e79398dcb6b8b7d6
george <heorhii.makeiev@gmail.com>
2021-10-03 21:32:16 +0300

    www

--- app.js
+++ app.js
@@ -21,3 +21,27 @@ fs.mkdir(mkdirPathOldWoman, {recursive: true}, (err) => {
 fs.mkdir(mkdirPathYoungerWoman, {recursive: true}, (err) => {
     console.log(err);
 });
+
+fs.appendFile(`${__dirname}/allUsers/manOlder20/Arseniy.txt`, 'Hello world22', (err)=>{
+    console.log(err);
+});
+
+const users = [
+    {name: "Sasha", gender: "male", age: 12},
+    {name: "Yulia", gender: "female", age: 25},
+    {name: "Oleh", gender: "male", age: 10},
+    {name: "Arseniy", gender: "male", age: 45},
+    {name: "Ivan", gender: "male", age: 19},
+    {name: "Olha", gender: "female", age: 23}
+];
+
+
+
+users.forEach(function (value,index) {
+    if (users.gender === 'female' && users.age > 20) {
+        fs.writeFile(`${__dirname}/allUsers/manOlder20`, JSON.stringify(users), (err) => {
+            console.log(err);
+        })
+
+    }
+})
