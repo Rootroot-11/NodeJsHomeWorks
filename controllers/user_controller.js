21ecabfb5eb40efe9662b93fd29dfd88f9329433
george <heorhii.makeiev@gmail.com>
2021-10-06 19:55:58 +0300

    node 2 hw

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,23 +1,49 @@
-const db = require('../dataBase/users.json');
+const db = require('../dataBase/allUsers.json');
+const fs = require('fs');
+const path = require("path");
+
+const pathUsers = path.join(__dirname, '../', 'dataBase', 'allUsers.json');
 
 module.exports = {
+
     getUsers: (req, res) => {
-        fs.readFile(db);
+        fs.readFile(path.join(pathUsers), (err, data) => {
+            if (err) {
+                console.log(err)
+                return
+            }
+            const user = JSON.parse(data.toString());
+
+            if (user.id === data.id) {
+                res.json(user);
+            }
+        });
     },
 
     getUserById: (req, res) => {
-        const { user_id } = req.params;
-
-        const user = db[user_id];
-        res.json({ user });
+        fs.readFile(path.join(pathUsers), (err, data) => {
+                if (err) {
+                    console.log(err)
+                    return
+                }
+                const users = JSON.parse(data.toString());
+                const {user_id} = req.params;
+                const user = users.find(user => user.id === +user_id);
+                res.json(user);
+            }
+        )
     },
+
     createUser: (req, res) => {
         console.log(req.body);
-
         db.push({...req.body, id: db.length + 1})
         res.json(db);
     },
+
     updateUser: (req, res) => {
         res.json('UPDATE USER');
     }
+
 }
+
+
--- dataBase/users.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-    {id: 1, name: 'Dima', age: 22},
-    {id: 2, name: 'Andrii', age: 12},
-    {id: 3, name: 'Katya', age: 70},
-    {id: 4, name: 'Alyona', age: 34},
-    {id: 5, name: 'Anna', age: 6}
-]
