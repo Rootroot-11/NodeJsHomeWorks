4a50ab7d38a9f203f5397e5200363a5bc4a5f320
george <heorhii.makeiev@gmail.com>
2021-10-12 14:04:01 +0300

    node 4 homework

--- middlewares/user.middleware.js
+++ middlewares/user.middleware.js
@@ -4,7 +4,7 @@ const userValidator = require('../validators/user.validators');
 module.exports = {
     createUserMiddleware: async (req, res, next) => {
         try {
-            const userByEmail = await User.findOne({ email: req.body.email });
+            const userByEmail = await User.findOne({email: req.body.email});
 
             if (userByEmail) {
                 throw new Error('Email already exist');
@@ -17,15 +17,32 @@ module.exports = {
         }
     },
 
-    isUserBodyValid:  (req, res, next) => {
+    isUserBodyValid: (req, res, next) => {
         try {
-            const { error, value } = userValidator.createUserValidator.validate(req.body);
+            const {error, value} = userValidator.createUserValidator.validate(req.body);
 
             if (error) {
                 throw new Error(error.details[0].message);
             }
 
             req.body = value;
+
+            next();
+        } catch (e) {
+            res.json(e.message);
+        }
+    },
+
+    doesUserExist: (req, res, next) => {
+        try {
+            const {user_id} = req.params;
+            const user = User.findById(user_id, {__v: 0});
+
+            if (!user) {
+                throw new Error("User with this ID does not exist");
+            }
+
+            req.user = user;
 
             next();
         } catch (e) {
