9a636b51ac81b9d9dc500b7feb6b1b8037cfef66
george <heorhii.makeiev@gmail.com>
2021-10-24 02:12:47 +0300

    node 7

--- configs/index.js
+++ configs/index.js
@@ -1,12 +1,11 @@
 const {MONGO_CONNECT_URL, PORT} = require('./config');
-const {PASSWORD_REGEXP, EMAIL_REGEXP, DEFAULT_STATUS_ERR} = require('./constans');
+const {PASSWORD_REGEXP, EMAIL_REGEXP, AUTHORIZATION} = require('./constans');
 const userRoles = require('./user-roles.enum');
-const AUTHORIZATION = require('./constans');
 const {WELCOME, ORDER_CONFIRMED, LOGIN, LOGOUT, DELETE, UPDATE} = require('./email-action.enum');
 
 module.exports = {
     MONGO_CONNECT_URL, PORT,
-    PASSWORD_REGEXP, EMAIL_REGEXP, DEFAULT_STATUS_ERR,
+    PASSWORD_REGEXP, EMAIL_REGEXP,
     userRoles, AUTHORIZATION,
     WELCOME, ORDER_CONFIRMED, LOGIN, LOGOUT, DELETE, UPDATE
 };
